fcd9a55becff313e14e9b6ad9b39e8dbaf3ad3ad
IIIBe2 <42515284+IIIBe2@users.noreply.github.com>
2020-08-17 21:37:41 +0900

    Add files via upload
    Added a counter for the number of additional characters and a copy button for double-clicking, as well as a button for intercepting keyboard input and changing the entered characters, as well as its animation, but the function does not work yet.

---
+++ Code_Converter_ASCII_and_add_to_u_letter_development-IIIBe2-patch-2/main.py
@@ -0,0 +1,38 @@
+import eel 
+import random
+
+eel.init('web', allowed_extensions=['.js', '.html'])
+
+def QuantityMod(Quantity, choices_simv):
+  Temp = ""
+  for x in range(Quantity):
+    Temp += "&#" + str(choices_simv[int(random.random() * len(choices_simv))]) + ";"
+
+  return Temp
+
+
+@eel.expose
+def get_recode(recoding_text, strf, Quantity):
+  choices_simv = list(range(768, 880))
+  res = ""
+  kl = 0
+  Teemp = []
+  Text = str(recoding_text)
+  Teemp = list(map(int, strf.split()))
+  try:
+    for q in Teemp:
+      choices_simv.remove(q)
+  except Exception: jk = 3
+   
+  for i in Text:
+    if i != ' ':
+      res+= i + QuantityMod(int(Quantity), choices_simv)
+    else:
+      if i == ' ':
+        res+= i
+
+  return res
+
+
+
+eel.start("main.html", size= (700, 1013))
